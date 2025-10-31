76aa0825b18a61e16d9742c35e1808339f4885ce
Will Wang <Will.Wang1@flashdot.com>
2025-10-31 11:08:16 +0800

    fix: host change

--- cmd/query/crypto.go
+++ cmd/query/crypto.go
@@ -86,7 +86,7 @@ func price(cmd *cobra.Command, args []string) (err error) {
 	if err != nil {
 		return
 	}
-	url := fmt.Sprintf("https://addons.wangsai.cloud/getPrice?exchange=%s&symbol=%s", exchange, symbol)
+	url := fmt.Sprintf("https://addons.willwang.cloud/getPrice?exchange=%s&symbol=%s", exchange, symbol)
 	resp, err := http.DefaultClient.Get(url)
 	if err != nil {
 		return
